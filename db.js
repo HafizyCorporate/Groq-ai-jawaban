0457bdab1924f7b9f97f2b28fc8cd06610b5150d
Teazha <hafizyazhar@gmail.com>
2026-02-12 17:35:50 +0700

    Update db.js

--- db.js
+++ db.js
@@ -2,13 +2,13 @@ const { Pool } = require('pg');
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-// Koneksi Postgres (Gunakan DATABASE_URL dari Railway)
+// Koneksi ke PostgreSQL Railway
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
 });
 
-// Tetap panggil SQLite lama untuk migrasi data
+// Tetap akses SQLite lama untuk ambil data riwayat jika perlu
 const dbPath = path.join(__dirname, 'db', 'database.sqlite');
 const sqliteDb = new sqlite3.Database(dbPath);
 
